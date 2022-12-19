83355f32d4572319ef6fa4071947e2a0d0cf72b6
salwaahmadz <salwaahmadz888@gmail.com>
2022-12-20 01:44:16 +0700

    Fixing url on EditPhoto.jsx and AddPhoto.jsx

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://salwaahmadz-galleryapp.netlify.app/" // TODO: replace this
 const studentName = "Salwa Ahmad Zanjabila" // TODO: replace this
 const studentId = "FE2632127" // TODO: replace this
 
